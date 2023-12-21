8638dfcab9d3c43bae1b7fa145646442c3b98e51
Abubakarl <Qsssoftnic@gmail.com>
2023-12-21 15:59:28 +0500

    fixed double app bar

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/user/SnapAway_background_eraser"
+export "FLUTTER_ROOT=/Users/abubakar/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/abubakar/Documents/Code playGround/SnapAway_background_eraser"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=2"
+export "FLUTTER_BUILD_NAME=1.0.2"
+export "FLUTTER_BUILD_NUMBER=4"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
